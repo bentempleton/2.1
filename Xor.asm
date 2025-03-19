038a774888841694820b308ad3d55196bca9f3de
bentempleton <kmhg5168@leeds.ac.uk>
2025-03-19 01:01:25 +0000

    d

--- Xor.asm
+++ Xor.asm
@@ -1,16 +1,28 @@
 @R0
 D=M
-@R1
-D=D|M
-@R2
-M=D
+@POSITIVE
+D;JGE
 
 @R0
 D=M
 @R1
-D=D&M
+M=-D
 @R2
-M=M-D
+M=1
+@R3
+M=0
+@END
+0;JMP
+
+(POSITIVE)
+@R0
+D=M
+@R1
+M=D
+@R2
+M=0
+@R3
+M=0
 
 (END)
 @END
