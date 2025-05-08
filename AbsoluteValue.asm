10868b0229cb70f01b4fcfca4d0d06dc9e609fcc
bentempleton <kmhg5168@leeds.ac.uk>
2025-05-08 19:58:27 +0000

    d

--- AbsoluteValue.asm
+++ AbsoluteValue.asm
@@ -1,29 +1,51 @@
+// value from R0 and place into the D register
 @R0
 D=M
+// do an OR operation between the D value from before and the value in R1
 @POSITIVE
 D;JGE
 
+// load R0 back into D 
 @R0
 D=M
+
+// negate the value and put it into R1
 @R1
 M=-D
+
+// assign R2 to 1 to flag that the value as negative 
 @R2
 M=1
+
+// assign R3 to 0
 @R3
 M=0
+
+// ignore the positive section and skip to the END
 @END
 0;JMP
 
 (POSITIVE)
+
+// load R0 again
 @R0
 D=M
+
+// store the postive value into R1
 @R1
 M=D
+
+// assign R2 to 0
 @R2
 M=0
+
+// assign R3 to 0
 @R3
 M=0
 
+// finish program
 (END)
+
+// infinite loop
 @END
 0;JMP
--- Xor.asm
+++ Xor.asm
@@ -1,17 +1,30 @@
+// get R0 value and put into D
 @R0
 D=M
+
+// do and OR operation between R1 and R0
 @R1
 D=D|M
+
+// put the OR result into R2
 @R2
 M=D
 
+// load R0 into the D again
 @R0
 D=M
+
+// do an AND operation between R0 and R1
 @R1
 D=D&M
+
+// take away the AND result from R2
 @R2
 M=M-D
 
+// stop the program
 (END)
+
+// infinite loop
 @END
 0;JMP
